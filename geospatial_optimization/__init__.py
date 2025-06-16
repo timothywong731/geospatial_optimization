caa63c11771930ff2d8e06f2d736ce957070d833
Timothy <shingyip56@gmail.com>
2025-06-16 23:08:17 +0100

    Add route plotting and demo

--- geospatial_optimization/__init__.py
+++ geospatial_optimization/__init__.py
@@ -1,6 +1,6 @@
 from .helpers import create_fan_polygon, get_grid_points_in_polygon_km, export_to_geojson
 from .optimization import optimize_sensor_placement, calculate_coverage_info
-from .plotting import plot_sensor_map
+from .plotting import plot_sensor_map, plot_sensor_routes
 from .mobile_optimization import select_scan_positions, plan_sensor_routes
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     "optimize_sensor_placement",
     "calculate_coverage_info",
     "plot_sensor_map",
+    "plot_sensor_routes",
     "select_scan_positions",
     "plan_sensor_routes",
 ]
--- geospatial_optimization/plotting.py
+++ geospatial_optimization/plotting.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
-from shapely.geometry import MultiPolygon
+from shapely.geometry import MultiPolygon, Point
 from mpl_toolkits.basemap import Basemap, cm, shiftgrid
 from netCDF4 import Dataset
 from shapely.geometry import Polygon
@@ -106,4 +106,78 @@ def plot_sensor_map(
     ]
     ax.legend(handles=legend_elements, loc="upper right")
     ax.set_title("Optimal Sensor Placement")
+    return ax
+
+
+def plot_sensor_routes(
+    operational_area: MultiPolygon,
+    routes: List[List[Point]],
+    ax: plt.Axes | None = None,
+) -> plt.Axes:
+    """Visualise mobile sensor routes on a geographical map.
+
+    Parameters
+    ----------
+    operational_area : MultiPolygon
+        Operational boundary to plot.
+    routes : list[list[Point]]
+        Output from :func:`plan_sensor_routes`.
+    ax : matplotlib.axes.Axes, optional
+        Existing axes to draw on.  If ``None`` a new figure is created.
+
+    Returns
+    -------
+    matplotlib.axes.Axes
+        Axes containing the completed plot.
+    """
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 7))
+
+    min_px = min(min(poly.exterior.xy[0]) for poly in operational_area.geoms)
+    min_py = min(min(poly.exterior.xy[1]) for poly in operational_area.geoms)
+    max_px = max(max(poly.exterior.xy[0]) for poly in operational_area.geoms)
+    max_py = max(max(poly.exterior.xy[1]) for poly in operational_area.geoms)
+
+    plot_margin = 3
+
+    m = Basemap(
+        projection="merc",
+        llcrnrlon=min_px - plot_margin,
+        urcrnrlon=max_px + plot_margin,
+        llcrnrlat=min_py - plot_margin,
+        urcrnrlat=max_py + plot_margin,
+        resolution="i",
+        ax=ax,
+    )
+
+    url = "http://ferret.pmel.noaa.gov/thredds/dodsC/data/PMEL/etopo5.nc"
+    etopodata = Dataset(url)
+    topoin = etopodata.variables["ROSE"][:]
+    lons = etopodata.variables["ETOPO05_X"][:]
+    lats = etopodata.variables["ETOPO05_Y"][:]
+    topoin, lons = shiftgrid(180.0, topoin, lons, start=False)
+    nx = int((m.xmax - m.xmin) / 10000.0) + 1
+    ny = int((m.ymax - m.ymin) / 10000.0) + 1
+    topodat = m.transform_scalar(topoin, lons, lats, nx, ny)
+    m.imshow(topodat, cm.GMT_haxby)
+
+    m.drawcoastlines()
+    m.drawcountries()
+    m.drawmapboundary()
+
+    for poly in operational_area.geoms:
+        px, py = poly.exterior.xy
+        mx, my = m(px, py)
+        xy = list(zip(mx, my))
+        p = plt.Polygon(xy, facecolor="none", edgecolor="blue", linewidth=2)
+        ax.add_patch(p)
+
+    for idx, route in enumerate(routes):
+        x = [pt.x for pt in route]
+        y = [pt.y for pt in route]
+        mx, my = m(x, y)
+        ax.plot(mx, my, marker="o", label=f"Sensor {idx + 1}")
+
+    ax.legend(loc="upper right")
+    ax.set_title("Planned Sensor Routes")
     return ax
